c1c9674c4f355a4dd56c0891ae6da238de8cc88c
Naomi <gabriella.naomi21@ui.ac.id>
2024-02-02 11:11:39 +0700

    HTML templates

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -2,9 +2,9 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
-
+import  java.util.ArrayList;
 import java.util.Iterator;
-
+import java.util.List;
 @Repository
 public class ProductRepository {
     private  List<Product> productData = new ArrayList<>();
