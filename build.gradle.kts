00c25344f2bc0244a8f441fae2594a302ef71561
Naomi <gabriella.naomi21@ui.ac.id>
2024-02-02 14:56:54 +0700

    preparation testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+val seleniumJavaVersion="4.14.1"
+val seleniumJupiterVersion="5.0.1"
+val webdrivermanagerVersion="5.6.3"
+val junitJupiterVersion = "5.9.1"
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
@@ -29,8 +33,31 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.seleniumhg.selenium:selenium-java:$seleniumJavaVersion")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+
+
+}
+tasks.register<Test>("unitTest"){
+	description="Runs unit tests."
+	group="verification"
+
+	filter{
+		excludeTestsMatching("*FunctionalTest")
+	}
 }
 
-tasks.withType<Test> {
+tasks.register<Test>("functionalTest"){
+	description="Runs functional tests."
+	group="verification"
+
+	filter {
+		includeTestsMatching("*FunctionalTest")
+	}
+}
+tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
 }
