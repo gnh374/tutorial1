9cf14d72131dea184d7e57b41f2b31fb5ef7379d
Naomi <gabriella.naomi21@ui.ac.id>
2024-02-02 10:23:20 +0700

    add productservice

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+    public Product create(Product product) {
+        productRepository.create(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        Iterator<Product> productIterator = productRepository.findAll();
+        List<Product> allProduct = new ArrayList<>();
+        productIterator.forEachRemaining(allProduct::add);
+        return allProduct;
+    }
+}
