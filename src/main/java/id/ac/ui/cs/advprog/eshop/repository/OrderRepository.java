a0c4facb305026a34729a8555a052edc0dfe057d
Naomi <gabriella.naomi21@ui.ac.id>
2024-03-04 22:33:54 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -1,16 +1,43 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import org.springframework.stereotype.Repository;
-import id.ac.ui.cs.advprog.eshop.model.Order
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class OrderRepository {
-    private List<Order> orrderData = new ArrayList<>();
-    public Order save(Order order){return null;}
-    public Order findById(String id){return null;}
-    public List<Order> findAllByAuthor(String author){return null;}
+    private List<Order> orderData = new ArrayList<>();
+    public Order save(Order order){
+        int i =0;
+        for (Order savedOrder :orderData){
+            if (savedOrder.getId().equals(order.getId())){
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i+=1;
+        }
+        orderData.add(order);
+        return order;
+    }
+    public Order findById(String id){
+        for (Order savedOrder : orderData){
+            if (savedOrder.getId().equals(id)){
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+    public List<Order> findAllByAuthor(String author){
+        List<Order> result = new ArrayList<>();
+        for (Order savedOrder : orderData){
+            if (savedOrder.getAuthor().equals(author)){
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
 
 
 
