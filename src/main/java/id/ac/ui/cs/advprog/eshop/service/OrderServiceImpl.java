9805df584c878577c6c1553e7b22c1596aa36f0e
Naomi <gabriella.naomi21@ui.ac.id>
2024-03-04 22:59:49 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -4,14 +4,34 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import java.util.NoSuchElementException;
 import java.util.List;
 
 public class OrderServiceImpl {
     @Autowired
     private OrderRepository orderRepository;
-    Order createOrder(Order order){return null;}
-    Order updateStatus(String orderId, String status){return null;}
-    Order findById(String orderId){return null;}
-    List<Order> findAllByAuthor(String id){return null;}
+    Order createOrder(Order order){
+        if (orderRepository.findById(order.getId())==null){
+            orderRepository.save(order);
+            return order;
+        }
+        return null;
+    }
+    Order updateStatus(String orderId, String status){
+        Order order = orderRepository.findById(orderId);
+        if (order != null) {
+            Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(), order.getAuthor(), status);
+            orderRepository.save(newOrder);
+            return  newOrder;
+        }else{
+            throw new NoSuchElementException();
+        }
+    }
+    Order findById(String orderId){
+        return orderRepository.findById(orderId);
+    }
+    List<Order> findAllByAuthor(String author){
+        return orderRepository.findAllByAuthor(author);
+    }
+
 }
